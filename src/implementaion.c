f52a4200772be7607d51f1213c89d12604ada330
Bhawanjeet Kaur <bhawanjeetgill@gmail.com>
2025-02-13 00:11:18 -0500

    Calling the handleArguments() function in main

--- src/implementaion.c
+++ src/implementaion.c
@@ -7,16 +7,6 @@
 #include "command_parser.h"
 
 int main(int argc, char *argv[]) {
-    // Parse command-line arguments
-    char mode[10];
-    char input_file[256];
-    char output_file[256];
-    
-    if (argc > 1) {
-        if (!parse_command_line(argc, argv, mode, input_file, output_file)) {
-        fprintf(stderr, "Invalid arguments. Usage: %s [-srec | -asm] <input_file>\n", argv[0]);
-        return EXIT_FAILURE;
-        }
-    }
+    return handleArguments(argc, argv);
 }
 
