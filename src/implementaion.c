189e7b417ae67092a4f96eb3ec01a7e7e1e3c0bf
Manreet Thind <Mthind2315@conestogac.on.ca>
2025-02-13 00:07:12 -0500

    Removed deleted content

--- src/implementaion.c
+++ src/implementaion.c
@@ -5,7 +5,6 @@
 #include "binary_to_srec.h"
 #include "binary_to_asm.h"
 #include "command_parser.h"
-#include "input_handler.h"
 
 int main(int argc, char *argv[]) {
     // Parse command-line arguments
@@ -19,31 +18,5 @@ int main(int argc, char *argv[]) {
         return EXIT_FAILURE;
         }
     }
-    else {
-    // No arguments provided; switch to interactive mode
-    printf("No command-line arguments detected. Switching to interactive mode.\n");
-    }
-      // Ask for mode
-      printf("Enter mode (-srec or -asm): ");
-      if (!read_user_input(mode, sizeof(mode))) {
-          printf("\nEOF detected. Exiting...\n");
-          return EXIT_SUCCESS;
-      }
-
-      // Validate mode
-      if (strcmp(mode, "-srec") != 0 && strcmp(mode, "-asm") != 0) {
-          fprintf(stderr, "Invalid mode. Use -srec or -asm.\n");
-          return EXIT_FAILURE;
-      }
-
-      // Ask for input file
-      printf("Enter input file name: ");
-      if (!read_user_input(input_file, sizeof(input_file))) {
-          printf("\nEOF detected. Exiting...\n");
-          return EXIT_SUCCESS;
-      }
-    
-    printf("Operation successful: %s -> %s\n", input_file, output_file);
-    return EXIT_SUCCESS;
 }
 
