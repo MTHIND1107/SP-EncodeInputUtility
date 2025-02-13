7164b3391d57e400cfd9740e6655ba70dd4734a7
Manreet Thind <Mthind2315@conestogac.on.ca>
2025-02-12 21:25:22 -0500

    Added header and function comments

--- inc/input_handler.h
+++ inc/input_handler.h
@@ -1,3 +1,11 @@
+/*
+ * FILE: input_handler.h
+ * PROJECT: encodeInput
+ * PROGRAMMER: MANREET THIND (STUDENT ID: 8982315)
+ * FIRST VERSION: 01/26/2025
+ * DESCRIPTION:
+ * Header file for input handling functions, including detecting CTRL-D (EOF).
+ */
 #ifndef INPUT_HANDLER_H
 #define INPUT_HANDLER_H
 
--- src/input_handler.c
+++ src/input_handler.c
@@ -1,7 +1,24 @@
+ /*
+ * FILE: input_handler.c
+ * PROJECT: encodeInput
+ * PROGRAMMER: MANREET THIND (STUDENT ID: 8982315)
+ * FIRST VERSION: 01/26/2025
+ * DESCRIPTION:
+ * This file contains the implementation of a function to handle user input,
+ * detecting when CTRL-D is pressed at the beginning of a new line.
+ */
  #include <stdio.h>
  #include <string.h>
  #include "../inc/input_handler.h"
  
+/*
+ * Name    : read_user_input
+ * Purpose : Reads user input while properly handling the EOF (CTRL-D) condition.
+ * Input   : buffer - char* - Buffer to store user input
+ *           max_length - int - Maximum length of the input
+ * Outputs : buffer is modified to store user input
+ * Returns : int - 1 if input is successfully read, 0 if EOF is detected
+ */
  int read_user_input(char *buffer, int max_length) {
      // Prompt user for input
      printf("Enter input (CTRL-D to exit): ");
