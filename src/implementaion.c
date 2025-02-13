fca3414a07cbf4b9bf6adcd80f0217ba0403c8a1
Manreet Thind <Mthind2315@conestogac.on.ca>
2025-02-13 00:13:40 -0500

    re updated main

--- src/implementaion.c
+++ src/implementaion.c
@@ -6,7 +6,9 @@
 #include "binary_to_asm.h"
 #include "command_parser.h"
 
-int main(int argc, char *argv[]) {
-    return handleArguments(argc, argv);
-}
-
+int main(int argc, char *argv[]) { 
+    if (handleArguments (argc, argv) != 0) { 
+         return EXIT_FAILURE; 
+    } 
+    return EXIT_SUCCESS; 
+} 
